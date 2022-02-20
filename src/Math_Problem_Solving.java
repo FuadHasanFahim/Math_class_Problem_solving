9aaca73b485961170160603e9da354d263265ef0
fhfahim <fhfahim116@gmail.com>
2022-02-20 17:31:43 +0600

    initial commit

---
+++ src/Math_Problem_Solving.java
@@ -0,0 +1,15 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
+public class Math_Problem_Solving {
+    public static void main(String[] args){
+        double d_num = 5.44444;
+        if ((d_num % 1) == 0) {
+            System.out.println("It's not a double number");
+        } else {
+            System.out.println("It's a double number");
+        }
+    }
+
+}
