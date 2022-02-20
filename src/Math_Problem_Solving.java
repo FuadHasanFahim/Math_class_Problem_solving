c06379aec1b9abd4b1d8a3954550866dc789de82
fhfahim <fhfahim116@gmail.com>
2022-02-20 17:56:42 +0600

    Take input from user and check it that it is a double or integer.

--- src/Math_Problem_Solving.java
+++ src/Math_Problem_Solving.java
@@ -4,8 +4,11 @@ import java.util.Scanner;
 
 public class Math_Problem_Solving {
     public static void main(String[] args){
-        double d_num = 5.44444;
-        if ((d_num % 1) == 0) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Inter Your Number");
+        double input = scanner.nextDouble();
+
+        if ((input % 1) == 0) {
             System.out.println("It's not a double number");
         } else {
             System.out.println("It's a double number");
