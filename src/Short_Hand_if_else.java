283a80973a344196506ab4337c15fc259c2fde09
fhfahim <fhfahim116@gmail.com>
2022-03-28 20:45:15 +0600

    Short Hand if else condition

---
+++ src/Short_Hand_if_else.java
@@ -0,0 +1,7 @@
+public class Short_Hand_if_else {
+    public static void main(String [] args){
+        int time = 20;
+        String result = (time<18) ? "Good Evening" : "Good Night";
+        System.out.println(result);
+    }
+}
