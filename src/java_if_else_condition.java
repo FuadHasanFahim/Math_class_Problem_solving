465da18299bee6c51675c22c5af0267b5873cef5
fhfahim <fhfahim116@gmail.com>
2022-03-28 13:07:13 +0600

    Complete if else condition

--- src/java_if_else_condition.java
+++ src/java_if_else_condition.java
@@ -1,2 +1,29 @@
 public class java_if_else_condition {
+    public static void main(String [] args){
+        if(20>18){
+            System.out.println("20 is greater than 18");
+        }
+
+        int a = 15;
+        int b = 20;
+        if(a<b){
+            System.out.println("15 is less than 20");
+        }
+        int time = 20;
+        if(time < 18){
+            System.out.println("Good Day");
+        }else {
+            System.out.println("Good Night");
+        }
+
+        int time2 = 20;
+         if(time2<18){
+             System.out.println("Good afterNoon");
+         }else if(time2<13){
+             System.out.println("Good Noon");
+        }else{
+             System.out.println("Good Evening");
+         }
+
+    }
 }
